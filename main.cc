7dc83e1b6123c99162571e7ad843d2844cb27ef1
gudali <43897624+gudali@users.noreply.github.com>
2018-10-06 12:54:26 -0700

    Add files via upload

--- main.cc
+++ main.cc
@@ -1,11 +1,6 @@
 #include <iostream>
-
-int main (int argc, char *argv[])
+using namespace std;
+int main ()
 {
-  if ( argc != 2 )
-    std::cout<<"usage: "<< argv[0] <<" <greetee>" << std::endl;
-  else {
-    std::string greetee = argv[1];
-    std::cout << "Hello " << greetee << std::endl;
-    }
+    cout << "hello world";
 }
